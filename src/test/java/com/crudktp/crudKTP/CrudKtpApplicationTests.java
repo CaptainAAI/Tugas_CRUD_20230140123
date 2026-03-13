2c09717ce8b78feac86a98cd8e052b9f08a0b94c
Ananda Agung Ismail <anandaagungismail@gmail.com>
2026-03-13 20:39:09 +0700

    first commit

---
+++ src/test/java/com/crudktp/crudKTP/CrudKtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudktp.crudKTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudKtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
