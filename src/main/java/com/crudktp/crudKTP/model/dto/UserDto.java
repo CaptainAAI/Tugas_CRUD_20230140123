67c494dc529aa533e39824c3e00006997387ddd9
Ananda Agung Ismail <anandaagungismail@gmail.com>
2026-03-13 22:52:50 +0700

    add user Dto carry data between different layers of an application

--- src/main/java/com/crudktp/crudKTP/model/dto/UserDto.java
+++ src/main/java/com/crudktp/crudKTP/model/dto/UserDto.java
@@ -1,4 +1,13 @@
 package com.crudktp.crudKTP.model.dto;
 
+import lombok.Data;
+
+@Data
 public class UserDto {
+    private int id;
+    private String nomorKTP;
+    private String nama;
+    private String alamat;
+    private String tanggalLahir;
+    private String jenisKelamin;
 }
